bbca37f08fc6502102fa4b45eedcb2a7ba04df6d
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-04-06 15:20:58 +0200

    add symlink creation to build scripts

--- build.cmd
+++ build.cmd
@@ -1,3 +1,6 @@
 @pushd %~dp0
 node-gyp configure build
+@if not exists mercury.node (
+	mklink mercury.node build\Release\mercury.node
+)
 @popd
--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 node-gyp configure build
+[ -r mercury.node ] || ln -s build/Release/mercury.node mercury.node
