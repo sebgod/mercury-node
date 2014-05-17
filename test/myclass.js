1ff5b0eba3ac8755e49ea00e27b766b010f50b20
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-05-17 11:32:36 +0200

    testing getClassName()

--- test/myclass.js
+++ test/myclass.js
@@ -4,14 +4,25 @@ var assert = require("assert");
 var java = require("java");
 java.classpath.push("test/src");
 
-var MyClass = java.import("com.nearinfinity.nodeJava.MyClass");
+var className = "com.nearinfinity.nodeJava.MyClass";
+var MyClass = java.import(className);
 
 describe('MyClass', function() {
     describe('#addNumbersSync', function() {
         it('should yield 3 when class loading works',
-        function() {
-            assert.equal(3, MyClass.addNumbersSync(1, 2));
-        })
+            function() {
+                assert.equal(3, MyClass.addNumbersSync(1, 2));
+            }
+        )
     })
+
+    describe('#getTypeName', function() {
+        it('should equal ' + className,
+            function() {
+                assert.equal(className, MyClass.getClassNameSync());
+            }
+        )
+    })
+
 });
 
--- test/src/com/nearinfinity/nodeJava/MyClass.java
+++ test/src/com/nearinfinity/nodeJava/MyClass.java
@@ -1,7 +1,10 @@
 package com.nearinfinity.nodeJava;
 
 public class MyClass {
-  public static int addNumbers(int a, int b) {
-    return a + b;
-  }
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+    public static String getClassName() {
+        return MyClass.class.getName();
+    }
 }
